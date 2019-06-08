2c91c38abb49913617a83740e40797f5ae00bff2
Tran Danh Tuan <end2end8x@gmail.com>
2019-06-09 03:52:04 +0700

    TURN

--- client/src/js/MyPeerConnection.js
+++ client/src/js/MyPeerConnection.js
@@ -2,7 +2,34 @@ import MediaDevice from './MediaDevice';
 import Emitter from './Emitter';
 import socket from './socket';
 
-const PC_CONFIG = { iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }] };
+const iceServers = { iceServers: [ 
+  { urls: ['stun:stun.l.google.com:19302'] },
+  {
+      url: 'turn:numb.viagenie.ca',
+      credential: 'muazkh',
+      username: 'webrtc@live.com'
+  },
+  {
+      url: 'turn:192.158.29.39:3478?transport=udp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
+  },
+  {
+      url: 'turn:192.158.29.39:3478?transport=tcp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
+  },
+  {
+      url: 'turn:turn.bistri.com:80',
+      credential: 'homeo',
+      username: 'homeo'
+  },
+  {
+      url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+      credential: 'webrtc',
+      username: 'webrtc'
+  }
+]};
 
 // ADD More TURN Server
 
@@ -10,7 +37,7 @@ class MyPeerConnection extends Emitter {
 
   constructor(friendID) {
     super();
-    this.rtcPeerConn = new RTCPeerConnection(PC_CONFIG);
+    this.rtcPeerConn = new RTCPeerConnection(iceServers);
     this.rtcPeerConn.onicecandidate = event => socket.emit('call', {
       to: this.friendID,
       candidate: event.candidate
